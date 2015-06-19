2ea756630a1ebd580881110aacd107ebb7a07359
Zoaib Khan <zoaib.ahmed@techlogix.com>
2015-06-19 16:55:15 +0500

    Lesson 3 Complete

--- app/src/main/java/com/example/zoaib/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/zoaib/sunshine/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.example.zoaib.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,7 +20,32 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+    private String mForecastStr;
+
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater)
+    {
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if(mShareActionProvider != null)
+        {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }else
+        {
+            Log.d(LOG_TAG,"Share Action Provider is null.");
+        }
+
     }
 
     @Override
@@ -27,11 +58,21 @@ public class DetailActivityFragment extends Fragment {
 
         if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT))
         {
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView)rootView.findViewById(R.id.detail_text);
-            textView.setText(forecastStr);
+            textView.setText(mForecastStr);
         }
 
         return rootView;
+    }
+
+    private Intent createShareForecastIntent()
+    {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
--- app/src/main/java/com/example/zoaib/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/zoaib/sunshine/ForecastFragment.java
@@ -248,6 +248,19 @@ public class ForecastFragment extends Fragment {
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedPrefs.getString(getString(R.string.pref_units_key),
+                    getString(R.string.pref_units_metric));
+            if(unitType.equals(getString(R.string.pref_units_imperial)))
+            {
+                high = (high*1.8)+32;
+                low = (low*1.8)+32;
+            }else if(!unitType.equals(getString(R.string.pref_units_metric)))
+            {
+                Log.d(LOG_TAG,"Unit type not found " + unitType);
+            }
+
+
             // For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
